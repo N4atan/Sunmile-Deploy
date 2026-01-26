937f0fd24d9b4f5cb90aea0be5c7b3cb6c159ff6
Felipe Cantini <114841536+fificici@users.noreply.github.com>
2026-01-26 18:33:29 -0300

    Update data-source.ts

--- Back-Sunmile/src/config/data-source.ts
+++ Back-Sunmile/src/config/data-source.ts
@@ -7,13 +7,17 @@ dotenv.config()
 const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env
 
 export const AppDataSource = new DataSource({
-	type: 'mysql',
-	host: DB_HOST,
-	port: Number(DB_PORT || '3306'),
-	username: DB_USERNAME,
-	password: DB_PASSWORD,
-	database: DB_NAME,
-	synchronize: true,
-	logging: true,
-	entities: ['src/models/*.ts']
+  type: 'mysql',
+  host: DB_HOST,
+  port: Number(DB_PORT || '3306'),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: [
+    process.env.NODE_ENV === 'production'
+      ? 'dist/models/*.js'   
+      : 'src/models/*.ts'    
+  ],
 })
